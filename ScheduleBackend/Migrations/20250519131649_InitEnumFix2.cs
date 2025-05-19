ff5ac9ea7a3c9d686aee258dc41fc41046a008f6
shulgigiri <nastasya.shylga.04@gmail.com>
2025-05-19 16:18:48 +0300

    исправила миграцию

---
+++ ScheduleBackend/Migrations/20250519131649_InitEnumFix2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScheduleBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitEnumFix2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Registrations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Registrations",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
